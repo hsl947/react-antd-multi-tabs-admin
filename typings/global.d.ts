beea5c13d9800489be16d924e9b23f2673b07495
hsl947 <1506070803@qq.com>
2020-07-01 21:21:18 +0800

    添加 `classnames` 声明

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -2,3 +2,8 @@
 /// <reference types="node" />
 /// <reference types="react" />
 /// <reference types="react-dom" />
+
+declare module "classnames" {
+  import classNames from 'classnames'
+  export default classNames
+}
