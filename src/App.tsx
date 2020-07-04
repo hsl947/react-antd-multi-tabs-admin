fc39ba08de395b20bf418230c541e27423e552b5
hsl947 <1506070803@qq.com>
2020-07-04 12:49:47 +0800

    修改部分为hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React, { FC } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Container from '@/pages/container'
 import Login from '@/pages/login'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/login" component={Login} />
-        <Route
-          path="/"
-          key="container"
-          render={(props) => <Container {...props} />}
-        />
-      </Router>
-    )
-  }
-}
+const App: FC = () => (
+  <Router>
+    <Route exact path="/login" component={Login} />
+    <Route
+      path="/"
+      key="container"
+      render={(props) => <Container {...props} />}
+    />
+  </Router>
+)
 
 export default App
